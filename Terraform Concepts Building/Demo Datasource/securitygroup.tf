12abcc8d30d977e7eaa6c884d0769cf020308a1d
Femotinga <femiadeliyi@gmail.com>
2025-02-03 16:43:32 -0500

    security groups

--- Terraform Concepts Building/Demo Datasource/securitygroup.tf
+++ Terraform Concepts Building/Demo Datasource/securitygroup.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "sg-custom_eu_west" {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        cidr_blocks = data.aws_ip_ranges.eu_west_ip_range.cidr_blocks
+        cidr_blocks = slice(data.aws_ip_ranges.eu_west_ip_range.cidr_blocks, 0, 50)
     }
 
     tags = {
