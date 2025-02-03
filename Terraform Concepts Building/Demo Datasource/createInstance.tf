7cb3a38f501d650cbcda45a2d76f5a420306a7d9
Femotinga <femiadeliyi@gmail.com>
2025-02-03 14:27:05 -0500

    demo datasource

--- Terraform Concepts Building/Demo Datasource/createInstance.tf
+++ Terraform Concepts Building/Demo Datasource/createInstance.tf
@@ -1,7 +1,20 @@
 data "aws_availability_zones" "availability" {}
 
-resource "aws_instance" "MyFirstInstnace" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Amazon Linux 2
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+}
+  filter {
+    name   = "virtualization-type"
+        values = ["hvm"]
+      }
+}
+
+resource "aws_instance" "MyFirstInstance" {
+  ami           = data.aws_ami.latest-ubuntu.id
   instance_type = "t2.micro"
   #availability_zone = data.aws_availability_zones.available.names[1]
   availability_zone = element(data.aws_availability_zones.availability.names, 1)
