fe53f91b66e1a3d69d467016768293fd64a41e3b
Femotinga <femiadeliyi@gmail.com>
2025-02-03 16:24:34 -0500

    security groups

---
+++ Terraform Concepts Building/Demo Datasource/securitygroup.tf
@@ -0,0 +1,23 @@
+data "aws_ip_ranges" "eu_west_ip_range" {
+    regions = ["eu-west-1","eu-west-2"]
+    service = ["EC2"]
+}
+
+resource "aws_security_group" "sg-custom_eu_west" {
+    name        = "sg-custom_eu_west"
+    description = "Example security group"
+    #vpc_id     = aws_vpc.custom_vpc.id
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = data.aws_ip_ranges.eu_west_ip_range.cidr_blocks
+    }
+
+    tags = {
+    CreateDate = data.aws_ip_ranges.eu_west_ip_range.create_date
+    SyncToken = data.aws_ip_ranges.eu_west_ip_range.sync_token
+    }
+
+}
