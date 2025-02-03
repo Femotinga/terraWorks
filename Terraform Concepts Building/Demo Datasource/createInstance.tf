32dc857c58c447742ccf986c3ab8cca0b413d20c
Femotinga <femiadeliyi@gmail.com>
2025-02-03 13:02:13 -0500

    demo datasource

--- Terraform Concepts Building/Demo Datasource/createInstance.tf
+++ Terraform Concepts Building/Demo Datasource/createInstance.tf
@@ -1,6 +1,10 @@
+data "aws_availability_zones" "availability" {}
+
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  #availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = element(data.aws_availability_zones.availability.names, 1)
   tags = {
     Name = "custom_instance"
   }
