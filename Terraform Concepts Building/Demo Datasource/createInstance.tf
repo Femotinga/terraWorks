dcd66f95bcd7aa9061d277d445c68f3071bcde1f
Femotinga <femiadeliyi@gmail.com>
2025-02-03 14:31:13 -0500

    demo datasource

--- Terraform Concepts Building/Demo Datasource/createInstance.tf
+++ Terraform Concepts Building/Demo Datasource/createInstance.tf
@@ -5,7 +5,7 @@ data "aws_ami" "latest-ubuntu" {
   owners      = ["099720109477"] # Amazon Linux 2
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
 }
   filter {
     name   = "virtualization-type"
